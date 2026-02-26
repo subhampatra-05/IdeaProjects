491a033ff8db2ee17130b95313b41638f549b4bd
subhampatra-05 <subhampatra0528@gmail.com>
2026-02-26 17:06:52 +0530

    Two HTML files and two servlets
    completed a project for 2 html file and 2 servlet

---
+++ TwohtmlandServlet/src/com/demo/Servlet1.java
@@ -0,0 +1,17 @@
+package com.demo;
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+public class Servlet1 extends HttpServlet {
+
+    protected void doPost(HttpServletRequest req, HttpServletResponse res)
+            throws ServletException, IOException {
+        res.setContentType("text/html");
+        PrintWriter out = res.getWriter();
+        out.println("<h2>Servlet 1 page</h2>");
+        out.println("<form action='servlet2' method='post'>");
+        out.println("<input type='submit' value='Go to Servlet2'>");
+        out.println("</form>");
+
+    }
+}
---
+++ TwohtmlandServlet/src/com/demo/Servlet2.java
@@ -0,0 +1,13 @@
+package com.demo;
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+public class Servlet2 extends HttpServlet{
+    protected void doPost(HttpServletRequest req, HttpServletResponse res)
+            throws ServletException, IOException {
+        res.setContentType("text/html");
+        PrintWriter out = res.getWriter();
+        out.println("<h2>Servlet 2 page</h2>");
+
+    }
+}
