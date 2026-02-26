8a38ab136ee83be8447043acff7cf5c029073525
subhampatra-05 <subhampatra0528@gmail.com>
2026-02-26 18:03:38 +0530

    Session Tracking
    Implemented session tracking.

--- TwohtmlandServlet/src/com/demo/Servlet1.java
+++ TwohtmlandServlet/src/com/demo/Servlet1.java
@@ -6,6 +6,13 @@ public class Servlet1 extends HttpServlet {
 
     protected void doPost(HttpServletRequest req, HttpServletResponse res)
             throws ServletException, IOException {
+
+        String user = req.getParameter("username");
+
+        // Create session
+        HttpSession session = req.getSession();
+        session.setAttribute("user", user);
+
         res.setContentType("text/html");
         PrintWriter out = res.getWriter();
         out.println("<h2>Servlet 1 page</h2>");
--- TwohtmlandServlet/src/com/demo/Servlet2.java
+++ TwohtmlandServlet/src/com/demo/Servlet2.java
@@ -5,9 +5,17 @@ import javax.servlet.http.*;
 public class Servlet2 extends HttpServlet{
     protected void doPost(HttpServletRequest req, HttpServletResponse res)
             throws ServletException, IOException {
+//        res.setContentType("text/html");
+//        PrintWriter out = res.getWriter();
+//        out.println("<h2>Servlet 2 page</h2>");
+
         res.setContentType("text/html");
         PrintWriter out = res.getWriter();
-        out.println("<h2>Servlet 2 page</h2>");
+
+        HttpSession session = req.getSession(true);
+        String user = (String) session.getAttribute("user");
+
+        out.println("<h2>Welcome " + user + "</h2>");
 
     }
 }
