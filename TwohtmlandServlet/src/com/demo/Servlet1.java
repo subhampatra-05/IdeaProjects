c03f896f07d8264980d16e49a5e4ca06537db8de
subhampatra-05 <subhampatra0528@gmail.com>
2026-02-27 18:03:04 +0530

    Added session tracking techniques
    implemented cookie for saving data, URL rewriting, and http session.

--- TwohtmlandServlet/src/com/demo/Servlet1.java
+++ TwohtmlandServlet/src/com/demo/Servlet1.java
@@ -1,7 +1,9 @@
 package com.demo;
+
 import java.io.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
+
 public class Servlet1 extends HttpServlet {
 
     protected void doPost(HttpServletRequest req, HttpServletResponse res)
@@ -9,16 +11,27 @@ public class Servlet1 extends HttpServlet {
 
         String user = req.getParameter("username");
 
-        // Create session
+        // COOKIE
+        Cookie c = new Cookie("user", user);
+        c.setMaxAge(3600);
+        res.addCookie(c);
+
+        // HTTP SESSION
         HttpSession session = req.getSession();
         session.setAttribute("user", user);
 
         res.setContentType("text/html");
         PrintWriter out = res.getWriter();
+
+        // CONTEXT PATH
+        String contextPath = req.getContextPath();
+
+        // URL + URL REWRITING
+        String servlet2URL = res.encodeURL(contextPath + "/servlet2");
+
         out.println("<h2>Servlet 1 page</h2>");
-        out.println("<form action='servlet2' method='post'>");
+        out.println("<form action='" + servlet2URL + "' method='post'>");
         out.println("<input type='submit' value='Go to Servlet2'>");
         out.println("</form>");
-
     }
 }
--- TwohtmlandServlet/src/com/demo/Servlet2.java
+++ TwohtmlandServlet/src/com/demo/Servlet2.java
@@ -1,21 +1,53 @@
 package com.demo;
+
 import java.io.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
-public class Servlet2 extends HttpServlet{
+
+public class Servlet2 extends HttpServlet {
+
     protected void doPost(HttpServletRequest req, HttpServletResponse res)
             throws ServletException, IOException {
-//        res.setContentType("text/html");
-//        PrintWriter out = res.getWriter();
-//        out.println("<h2>Servlet 2 page</h2>");
 
         res.setContentType("text/html");
         PrintWriter out = res.getWriter();
 
-        HttpSession session = req.getSession(true);
-        String user = (String) session.getAttribute("user");
+        out.println("<h2>Servlet 2 page</h2>");
 
-        out.println("<h2>Welcome " + user + "</h2>");
+        /* ================= COOKIE ================= */
+        Cookie[] cookies = req.getCookies();
+        boolean cookieFound = false;
 
+        if (cookies != null) {
+            for (Cookie c : cookies) {
+                if (c.getName().equals("user")) {
+                    out.println("<p>Welcome " + c.getValue() + "</p>");
+                    out.println("<p><b>Data saved using COOKIE</b></p>");
+                    cookieFound = true;
+                }
+            }
+        }
+
+        if (!cookieFound) {
+            out.println("<p><b>No user cookie received</b></p>");
+        }
+
+        /* ================= HTTP SESSION ================= */
+        HttpSession session = req.getSession(false);
+
+        if (session != null) {
+            String user = (String) session.getAttribute("user");
+            out.println("<p>Welcome " + user + "</p>");
+            out.println("<p><b>Data saved using HTTP SESSION</b></p>");
+
+            /* ================= URL REWRITING CHECK ================= */
+            if (req.isRequestedSessionIdFromURL()) {
+                out.println("<p><b>Session tracked using URL REWRITING</b></p>");
+            } else {
+                out.println("<p><b>Session tracked using COOKIE (JSESSIONID)</b></p>");
+            }
+        } else {
+            out.println("<p><b>No active session found</b></p>");
+        }
     }
 }
