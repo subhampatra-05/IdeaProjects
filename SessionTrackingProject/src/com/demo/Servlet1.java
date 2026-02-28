fdfae81878333d22cffe3bde968657ce759c7f85
subhampatra-05 <subhampatra0528@gmail.com>
2026-02-28 15:04:51 +0530

    Added inputs
    added firstname, lastname, city as inputs

--- SessionTrackingProject/src/com/demo/Servlet1.java
+++ SessionTrackingProject/src/com/demo/Servlet1.java
@@ -9,14 +9,18 @@ public class Servlet1 extends HttpServlet {
     protected void doPost(HttpServletRequest req, HttpServletResponse res)
             throws ServletException, IOException {
 
-        String user = req.getParameter("username");
+        String firstname = req.getParameter("firstname");
+        String lastname  = req.getParameter("lastname");
+        String city      = req.getParameter("city");
 
-        Cookie c = new Cookie("cookieUser", user);
-        c.setMaxAge(3600);
-        res.addCookie(c);
+        res.addCookie(new Cookie("c_firstname", firstname));
+        res.addCookie(new Cookie("c_lastname", lastname));
+        res.addCookie(new Cookie("c_city", city));
 
         HttpSession session = req.getSession();
-        session.setAttribute("sessionUser", user);
+        session.setAttribute("s_firstname", firstname);
+        session.setAttribute("s_lastname", lastname);
+        session.setAttribute("s_city", city);
 
         res.setContentType("text/html");
         PrintWriter out = res.getWriter();
@@ -26,7 +30,11 @@ public class Servlet1 extends HttpServlet {
         out.println("<h2>Servlet1 Page</h2>");
 
         out.println("<form action='" + url + "' method='post'>");
-        out.println("<input type='hidden' name='hiddenUser' value='" + user + "'>");
+
+        out.println("<input type='hidden' name='h_firstname' value='" + firstname + "'>");
+        out.println("<input type='hidden' name='h_lastname' value='" + lastname + "'>");
+        out.println("<input type='hidden' name='h_city' value='" + city + "'>");
+
         out.println("<input type='submit' value='Go to Servlet2'>");
         out.println("</form>");
     }
--- SessionTrackingProject/src/com/demo/Servlet2.java
+++ SessionTrackingProject/src/com/demo/Servlet2.java
@@ -14,41 +14,36 @@ public class Servlet2 extends HttpServlet {
 
         out.println("<h2>Servlet2 Page</h2>");
 
+        out.println("<h3>Using Cookies</h3>");
         Cookie[] cookies = req.getCookies();
-        boolean cookieFound = false;
-
         if (cookies != null) {
             for (Cookie c : cookies) {
-                if (c.getName().equals("cookieUser")) {
-                    out.println("<p>Welcome " + c.getValue() + "</p>");
-                    out.println("<b>Data via COOKIE</b><br>");
-                    cookieFound = true;
+                if (c.getName().startsWith("c_")) {
+                    out.println(c.getName() + " : " + c.getValue() + "<br>");
                 }
             }
-        }
-        if (!cookieFound) {
-            out.println("<b>No Cookie Data</b><br>");
+        } else {
+            out.println("No cookies found<br>");
         }
 
+        /* ========= HTTP SESSION ========= */
+        out.println("<h3>Using HttpSession</h3>");
         HttpSession session = req.getSession(false);
         if (session != null) {
-            String user = (String) session.getAttribute("sessionUser");
-            out.println("<p>Welcome " + user + "</p>");
-            out.println("<b>Data via HTTP SESSION</b><br>");
+            out.println("First Name: " + session.getAttribute("s_firstname") + "<br>");
+            out.println("Last Name: " + session.getAttribute("s_lastname") + "<br>");
+            out.println("City: " + session.getAttribute("s_city") + "<br>");
 
             if (req.isRequestedSessionIdFromURL()) {
-                out.println("<b>Session tracked using URL REWRITING</b><br>");
+                out.println("<b>Session tracked using URL Rewriting</b><br>");
             } else {
                 out.println("<b>Session tracked using COOKIE (JSESSIONID)</b><br>");
             }
-        } else {
-            out.println("<b>No Session Data</b><br>");
         }
 
-        String hiddenUser = req.getParameter("hiddenUser");
-        if (hiddenUser != null) {
-            out.println("<p>Welcome " + hiddenUser + "</p>");
-            out.println("<b>Data via HIDDEN FORM FIELD</b><br>");
-        }
+        out.println("<h3>Using Hidden Form Fields</h3>");
+        out.println("First Name: " + req.getParameter("h_firstname") + "<br>");
+        out.println("Last Name: " + req.getParameter("h_lastname") + "<br>");
+        out.println("City: " + req.getParameter("h_city") + "<br>");
     }
 }
